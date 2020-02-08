23144b3b99632c3d77d2d02f0e95aad700216f09
pgorka1 <night.knight12358@gmail.com>
2020-02-08 20:30:01 +0100

    Updates

---
+++ precache-manifest.1067f120387fe51621061c342a093a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e472883c17961e4bf23f478a3c42c41d",
+    "url": "/peerjs/index.html"
+  },
+  {
+    "revision": "a923169d7dbcd7b6147c",
+    "url": "/peerjs/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2916709bdd73c31efc1c",
+    "url": "/peerjs/static/js/2.6a2a7794.chunk.js"
+  },
+  {
+    "revision": "a923169d7dbcd7b6147c",
+    "url": "/peerjs/static/js/main.2f08e0ec.chunk.js"
+  },
+  {
+    "revision": "14973f2a11521b5f209a",
+    "url": "/peerjs/static/js/runtime-main.4254ddbc.js"
+  }
+]);
